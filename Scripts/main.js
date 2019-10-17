3e27c996bcb0b016f408827f8d15b8b26a36d215
Justin Mecham <justin@mecham.me>
2019-10-17 01:52:34 +0000

    Cleanups and restructuring.

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,31 +6,57 @@
 // Copyright © 2019 Justin Mecham. All rights reserved.
 // 
 
-let watcher;
+class RuboCopIssuesProvider {
 
-exports.activate = function() {
-    // Do work when the extension is activated
-    // TODO: Start the file watcher
-    
-    watcher = nova.fs.watch("*.rb")
-}
-
-exports.deactivate = function() {
-    // Clean up state before the extension is deactivated
-}
-
-// let currentIssues = [];
-
-class RuboCopRunner {
     constructor() {
+        this.editors = [];
+        this.offenses = {};
+    }
+
+    addTextEditor(editor) {
+        const document = editor.document;
+
+        if (document.syntax != "ruby") {
+            console.log(`[addTextEditor] Skipping ${document.path} (Syntax: ${document.syntax})`);
+            return;
+        }
+
+        console.log(`[addTextEditor] Adding ${document.path} (Syntax: ${document.syntax})`);
+
+        const relativePath = nova.workspace.relativizePath(document.path);
+        this.processFile(relativePath);
+
+        editor.onDidDestroy(this.removeTextEditor.bind(this));
+        this.editors.push(editor);
+    }
+    
+    removeTextEditor(editor) {
+        const index = this.editors.indexOf(editor);
+        if (index) {
+            const document = editor.document;
+            console.log(`[removeTextEditor] Removing editor for ${document.path}`);
+            this.editors.splice(this.editors.index, 1);
+            delete this.offenses[editor.document.path];
+        } else {
+            console.log.warn("[removeTextEditor] Attempted to remove an unknown text editor...")
+        }
+    }
+
+    provideIssues(editor) {
+                const relativePath = nova.workspace.relativizePath(editor.document.path);
+
+        // console.log("FOO", editor.document.path, JSON.stringify(this.offenses));
+        if (!this.offenses[relativePath]) return;
+        return this.offenses[relativePath].map(offense => offense.issue);
     }
 
     processFile(path) {
-        let options = {
-            args: ["rubocop", "-fj", path]
+        const options = {
+            args: ["rubocop", "-fj", path],
+            cwd: nova.workspace.path
         };
 
-        let process = new Process("/usr/bin/env", options);
+        const process = new Process("/usr/bin/env", options);
 
         process.onStdout(this.processResult.bind(this));
 
@@ -44,63 +70,72 @@ class RuboCopRunner {
     processResult(result) {
         const parsedResult = JSON.parse(result);
         console.log("processResult ", result);
-        return parsedResult;
+
+        const path = parsedResult["files"][0]["path"]; // TODO: WTF?! The results I'm getting back from RuboCop are stripping off "/U" from the beginning of the path...
+        const rawOffenses = parsedResult["files"][0]["offenses"];
+        const offenses = rawOffenses.map(rawOffense => new RuboCopOffense(rawOffense))
+
+        this.offenses[path] = offenses;
     }
 }
 
-class IssuesProvider {
-    constructor() {
-        this.runner = new RuboCopRunner();
+class RuboCopOffense {
+    
+    constructor(rawOffense) {
+        this.severity = rawOffense["severity"];
+        this.cop = rawOffense["cop_name"];
+        this.message = rawOffense["message"];
+        this.startColumn = rawOffense["location"]["start_column"];
+        this.startLine = rawOffense["location"]["start_line"];
+        this.stopColumn = rawOffense["location"]["last_column"];
+        this.stopLine = rawOffense["location"]["last_line"];
     }
     
-    provideIssues(editor) {
-        let issues = [];
+    get issue() {
+        const issue = new Issue();
 
-        console.log("provideIssues", editor.document.path);
-        
-        let results = this.runner.processFile(editor.document.path);
+        issue.source = "RuboCop";
+        issue.code = this.cop;
 
-console.log("results:", results);
-        if (results && results["files"]) {
-            
-            results["files"].forEach((file) => {
-                console.log("file:", file);
-                file["offenses"].forEach((offense) => {
-                    console.log("Offense:", offense, offense["message"]);
-    
-                    let issue = new Issue();
-            
-                    let message = offense["message"];
-                    message = message.replace(`${offense["cop_name"]}: `, "")
-                    
-                    issue.message = message;
-                    
-                    let severity = offense["severity"];
-                    if (severity == "warning") {
-                        issue.severity = IssueSeverity.Warning;
-                    } else if (severity == "error" || severity == "fatal") {
-                        issue.severity = IssueSeverity.Error;
-                    } else if (severity == "refactor" || severity == "convention") {
-                        issue.severity = IssueSeverity.Hint;
-                    }
-    
-                    issue.line = offense["location"]["start_line"];
-                    issue.endLine = offense["location"]["last_line"]
-                    issue.column = offense["location"]["start_column"];
-                    issue.endColumn = offense["location"]["last_column"];
-                    issue.source = "RuboCop";
-                    issue.code = offense["cop_name"];
-    
-                    issues.push(issue);
-                })
-            });
+        const cleanedMessage = this.message.replace(`${this.cop}: `, "");
+        issue.message = cleanedMessage;
+
+        if (this.severity == "warning") {
+            issue.severity = IssueSeverity.Warning;
+        } else if (this.severity == "error" || this.severity == "fatal") {
+            issue.severity = IssueSeverity.Error;
+        } else if (this.severity == "refactor" || this.severity == "convention") {
+            issue.severity = IssueSeverity.Hint;
         }
 
-        return issues;
+        issue.line = this.startLine;
+        issue.endLine =  this.stopLine;
+        issue.column = this.startColumn;
+        issue.endColumn = this.stopColumn;
+
+        return issue;
     }
-    
+
 }
 
 
-nova.assistants.registerIssueAssistant("ruby", new IssuesProvider());
+// ----------------------------------------------------------------------------
 
+let disposables;
+
+exports.activate = function() {
+    console.log("Activating");
+
+    disposables = new CompositeDisposable();
+
+    const issuesProvider = new RuboCopIssuesProvider();
+    nova.workspace.onDidAddTextEditor(issuesProvider.addTextEditor.bind(issuesProvider));
+
+    disposables.add(nova.assistants.registerIssueAssistant("ruby", issuesProvider));
+}
+
+exports.deactivate = function() {
+    console.log("Deactivating");
+    
+    disposables.dispose();
+}
